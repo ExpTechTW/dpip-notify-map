6baef9abc7bd0760641d8aab6a2ead4d2b1c62ca
YuYu1015 <whes1015@gmail.com>
2025-07-31 13:33:57 +0800

    Revert "fix: path"
    This reverts commit 4205244fef431f20d9dce1637d882933ec28a770.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-const basePath = isProduction ? `/history` : "";
-
 const nextConfig: NextConfig = {
-  basePath: basePath,
-  assetPrefix: basePath,
   output: "standalone",
   images: {
     unoptimized: true,
