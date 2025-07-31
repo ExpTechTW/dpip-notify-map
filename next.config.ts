4205244fef431f20d9dce1637d882933ec28a770
YuYu1015 <whes1015@gmail.com>
2025-07-31 13:08:28 +0800

    fix: path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? `/history` : "";
+
 const nextConfig: NextConfig = {
+  basePath: basePath,
+  assetPrefix: basePath,
   output: "standalone",
   images: {
     unoptimized: true,
